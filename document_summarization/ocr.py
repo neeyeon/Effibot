d778c3f7009f0b97a435d8381335041018e6d7f1
Neeharika Kamlanker <98646320+neeyeon@users.noreply.github.com>
2024-12-04 15:47:51 +0530

    Add files via upload

---
+++ document_summarization/ocr.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def perform_ocr(image_path):
+    try:
+        with Image.open(image_path) as img:
+            text = pytesseract.image_to_string(img)
+            return text
+    except Exception as e:
+        print(f"Error during OCR: {e}")
+        return None
+
